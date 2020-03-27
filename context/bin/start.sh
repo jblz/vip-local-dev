2fcb00f35de9a1478a76ac8ed3b66c086e27bace
Jeff Bowen <jblz@users.noreply.github.com>
2020-03-27 14:05:46 -0400

    use .env file

--- context/bin/start.sh
+++ context/bin/start.sh
@@ -4,20 +4,21 @@ echo "Waiting on db to be ready...";
 sh /usr/local/bin/wait-for db:3306 -t 30 || exit 1;
 
 if $(wp core is-installed); then
-    echo "WordPress is already installed";
-    if $VIP_GO_EMPTY_SITE; then
-        echo "Emptying site";
-        wp site empty --yes;
-    else
-        exit;
-    fi
+    echo "WordPress is already installed. 🚀";
+    exit;
 fi
 
-echo VIP_GO_MULTISITE is $VIP_GO_MULTISITE;
+ADMIN_EMAIL=${WPVIP_ADMIN_EMAIL:-"nobody@example.com"}
+ADMIN_USER=${WPVIP_ADMIN_USER:-admin}
+ADMIN_PASS=${WPVIP_ADMIN_PASS:-password}
+SITE_TITLE=${WPVIP_SITE_TITLE:-Just Another VIP Test Site}
+SITE_URL=${WPVIP_SITE_URL:-"http://localhost"}
 
-if [[ -z "$VIP_GO_MULTISITE" ]] || [[ "$VIP_GO_MULTISITE" -eq "0" ]] || [[ ! $VIP_GO_MULTISITE ]]; then
+if [[ -z "$WPVIP_MULTISITE" ]] || [[ "$WPVIP_MULTISITE" -eq "0" ]] || [[ ! $WPVIP_MULTISITE ]]; then
     echo "Installing WordPress";
-    wp core install --url="http://localhost" --title="vip-local-dev test" --admin_user="admin" --admin_password="password" --admin_email="nobody@example.com";
+    wp core install --url="$SITE_URL" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASS" --admin_email="$ADMIN_EMAIL";
+    echo RESULT: $?
+    echo SITE_URL is $SITE_URL;
 else
     echo "Installing WordPress MULTISITE";
     echo "TODO";
